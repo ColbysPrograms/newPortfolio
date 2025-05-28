634d93f34b9b7ee4dc93fc27f43d4746b2508b49
Colby <colby.cox2020@gmail.com>
2025-05-28 18:16:15 -0500

    Changing base

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,7 +11,7 @@ function About() {
       <div className="flex flex-col bg-gray-200 p-5 mr-5 rounded-md">
         <div className="lg:flex mb-3 justify-center">
           <div className="flex justify-center min-w-max">
-            <img src="/gta.avif" height={size} width={size}></img>
+            <img src="gta.avif" height={size} width={size}></img>
           </div>
           <p className="lg:ml-3">
             I'm currently a Graduate Teaching Assistant for the University of Central Arkansas, where I'm getting my Master's degree in Applied Mathematics. I received my Bachelor's degree in Computer Science from UCA in December 2024. As of Summer 2025, I have assisted with College Algebra and Applied Calculus. I taught for three sessions of College Algebra and Applied Calculus each, writing and reviewing my own presentations for each. My presentations for Applied Calculus were to show computer science students some of the overlap in mathematics and computer science. These were the <a href="https://github.com/ColbysPrograms/balloonProject" target="_blank"><i>Balloon Project</i></a> and <a href="https://github.com/ColbysPrograms/NewtonPresentation" target="_blank"><i>Newton's Method</i></a> projects shown on the Projects page. I've highly enjoyed the process of teaching students about these intersections of knowledge and hope to do the same during the Fall 2025 semester when I will be fully teaching a College Algebra class.
@@ -22,12 +22,12 @@ function About() {
             Outside of my educational career, I have many hobbies both inside and outside of my profession. On the right is an ordered chart of my favorite games. I enjoy a wide variety of genres, and the only real consistency between all of my picks is having a high degree of player expression. Another one of my hobbies is playing music. I performed in the UCA Marching Band and Concert Band for three years playing trumpet, and regularly play my piano. I'd hope to one day incorporate that experience in making my own music, like when I'm improvising on my piano. My least predictable hobby is my love for dance. I've attended dance classes, mainly Zumba, several times a week for years. My final hobby I'd like to share is baking. I love to bake for myself and my family. For Mother's Day, I went home and delivered my homemade chocolate chocolate chunk cookies to my Mom and my grandmothers. I hope this has been enlightening on my interests in and out of my career and given a more full idea of who I am.
           </p>
           <div className="min-w-max flex justify-center">
-            <img src="/chart.avif" height={size} width={size}></img>
+            <img src="chart.avif" height={size} width={size}></img>
           </div>
         </div>
       </div>
       <div className="flex justify-center mt-3">
-        <a href="/resume.pdf" target="_blank" download="Colby Cox's Resume" className="">
+        <a href="resume.pdf" target="_blank" download="Colby Cox's Resume" className="">
           <p className="text-4xl">Download Resume</p>
         </a>
       </div>
